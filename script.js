206d88157d328d9215e6d94cdbbf7e81736d0faa
Druvasena Reddy <66788819+druva-06@users.noreply.github.com>
2023-07-29 02:59:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+
+let ele = document.getElementById('level')
+let count = 0
+while(ele.parentElement){
+    ele = ele.parentElement
+    count++
+}
+
+alert('The level of the element is: '+count)
